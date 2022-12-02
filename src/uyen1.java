4a749ba7b2bbc5dcae37ed30613bce515c382f92
hung.nguyenvan <nvhung0207002@gmail.com>
2022-12-02 22:31:52 +0700

    UyenHP commit

--- src/uyen1.java
+++ src/uyen1.java
@@ -4,6 +4,7 @@ public class uyen1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Hùng"); // prints Hello World
+		System.out.println("Hello UyenHP. Love uuu"); // prints Hello World
 	}
 
 }
